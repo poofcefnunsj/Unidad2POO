f73f8f229a2544eba665fc20d48efad77287eb2e
FCEFN-UNSJ <63244005+poofcefnunsj@users.noreply.github.com>
2020-04-14 22:02:09 -0300

    Update destructorCicloDeVida.py

--- destructorCicloDeVida.py
+++ destructorCicloDeVida.py
@@ -5,12 +5,14 @@ Created on Wed Jan 22 07:35:11 2020
 @author: morte
 """
 class A:
+    __b=None
     def __init__(self, unObjetoB):
         self.__b=unObjetoB
     def __del__(self):
         print('Chau objeto A')
 
 class B:
+    __a=None
     def __init__(self):
         self.__a=A(self)
     def __del__(self):
@@ -21,11 +23,12 @@ def funcionCreaB():
     del b
     
 class CicloDeVida:
+    __nombre=None
     def __init__(self, nombre):
         print('Hola: ',nombre)
-        self.nombre = nombre
+        self.__nombre = nombre
     def vida(self):
-        print(self.nombre)
+        print(self.__nombre)
     def __del__(self):
         print('Chau... ',self.nombre)
 def funcion():
